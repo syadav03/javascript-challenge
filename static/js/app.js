40014d0c3fb35fb4eb1b1ed437faebe8026a06ba
Sherali <sheraliyadav1993@gmail.com>
2021-04-04 22:12:36 -0400

    final code

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,54 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Using UFO data
+
+var tbody = d3.select("tbody");
+
+// view table 
+
+data.forEach((ufo_info) => {
+    var row = tbody.append("tr");
+    Object.entries(ufo_info).forEach(([Key,value]) => {
+       var cell = row.append("td");
+       cell.text(value);
+    });
+});
+
+// Select button
+var button = d3.select("#filter-btn");
+
+//select form 
+var form = d3.select("#form");
+
+//create handlers
+button.on("click",runEnter);
+
+// complete function for form
+
+function runEnter(){
+    // stop page from refreshing 
+    d3.event.preventDefault();
+
+    //select input element 
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+    var filteredData = data.filter(ufo => ufo.datetime === inputValue);
+    console.log(filteredData);
+    // remove from list
+
+    tbody.html("");
+
+    // view table 
+    filteredData.forEach((ufo_info) => {
+        var row = tbody.append("tr");
+        Object.entries(ufo_info).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
